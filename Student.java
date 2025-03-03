9cd18c9fe765c7aaccaff14e683c30798a61689a
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-03 12:31:49 +0530

    Adding Attributes
    Defining the Constructor and Attributes of the Class Student as Name, PRN, Position.

--- Student.java
+++ Student.java
@@ -1,5 +1,39 @@
 //Student.java
 
 class Student{
+		private String name;
+		private long prn;
+		private int Position;
+		
+		public Student(String name, long prn, int Position){
+			setName(name);
+			setPRN(prn);
+			setPosition(Position);
+		}
+		
+		public void setName(String name){
+			this.name = name;
+		}
+		
+		public String getName(){
+			return this.name;
+		}
+		
+		public void setPRN(long prn){
+			this.prn = prn;
+		}
+		
+		public long getPRN(){
+			return this.prn;
+		}
+		
+		public void setPosition(int Position){
+			this.Position = Position;
+		}
+		
+		public int getPosition(){
+			return this.Position;
+		}
+		
 		
 }
