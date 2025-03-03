19551c737d22f81e2564b39d9df5f54bdd8e5a1f
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-03 22:22:47 +0530

    Add StudentOperations class with ArrayList methods

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,2 +1,9 @@
+/*
+ * StudentOperations.java
+ * Description: This file contains methods to perform operations on an ArrayList of Student objects.
+ */
+
+import java.util.ArrayList;
+
 public class StudentOperations {
 }
